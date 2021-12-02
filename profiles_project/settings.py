2674cd56823a08c1252218bee5def8cf531bc93d
Paul Lachaud <paul.lachaud@outlook.com>
2021-12-02 01:01:45 -0500

    update allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://ec2-3-82-241-159.compute-1.amazonaws.com/',
+    '127.0.0.1'
+]
 
 
 # Application definition
